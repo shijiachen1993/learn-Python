355dedb7b58af5bb2e0c37a60d219d7211e49fd4
shijiachen1993 <jiachen_shi@163.com>
2018-03-15 16:23:19 +0800

    modify

--- class/student.py
+++ class/student.py
@@ -1,24 +1,23 @@
 class Student(object):
     
-	def __init__(self,name,score):
-	    self.__name=name
-		self.__score=score
+	def __init__(self, name, score):
+	    self.__name = name
+	    self.__score = score
 	def get_name(self):
 	    return self.__name
-	def set_name(self,name):
-	    self.__name=name
+	def set_name(self, name):
+	    self.__name = name
 	def get_score(self):
-	    return __score
+	    return self.__score
 	def set_score(self,score):
-	    self.__score=score
-def main()
-    bart = Student('Bart', 'male')
-    if bart.get_gender() != 'male':
-        print('测试失败!')
-    else:
-        bart.set_gender('female')
-        if bart.get_gender() != 'female':
-            print('测试失败!')
-        else:
-            print('测试成功!') 
-		
+	    self.__score = score
+bart = Student('Bart', 60)
+if bart.get_score() != 60:
+	print('测试失败!')
+else:
+	bart.set_score(90)
+	if bart.get_score() != 90:
+		print('测试失败!')
+	else:
+		print('测试成功!') 
+	
